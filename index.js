0e87d2a6628fca3f0a2633d52c9d51af56f40d5f
Nguyễn Thanh Mài <104611730+blackdzcuto@users.noreply.github.com>
2022-06-07 21:53:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const logger = require("./utils/log");
 
 const dashboard = http.createServer(function(_req, res) {
   res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-  res.write("HI! Chào Admin Khánh");
+  res.write("HI! Chào Admin BLACK");
   res.end();
 });
 
